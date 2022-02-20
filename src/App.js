b9948a25d80df294b0a60aef29be94444d711cbf
PedroHenrique431 <pedrohenriquebgs1@gmail.com>
2022-02-20 13:38:07 -0300

    .

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
+import React from "react";
+import GlobalStyle from './globalStyles';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+//Pages
+import Home from './pages/Home';
+import SignUp from './pages/SignupPage';
+import Pricing from './pages/PricingPage';
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <Router>
+			<GlobalStyle />
+			<Switch>
+				<Route path="/" exact component={Home} />
+				<Route path="/signup" exact component={SignUp} />
+				<Route path="/pricing" exact component={Pricing} />
+			</Switch>
+		</Router>
   );
 }
 
