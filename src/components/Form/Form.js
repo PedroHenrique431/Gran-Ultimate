29df577a8ff5c43f06bb11fa559575cc6c303dea
PedroHenrique431 <pedrohenriquebgs1@gmail.com>
2022-02-22 13:09:26 -0300

    .

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -8,36 +8,141 @@ import {
   FormTitle,
   FormLabel,
 } from "./FormStyles";
+
+import { useState } from "react";
 import { Container } from "../../globalStyles";
 
 const Form = () => {
+  const [data, setData] = useState({
+    Nome: "",
+    telefone: "",
+    Idade: "",
+    conheceCDZ: "",
+    experiencia: "",
+    nomePersonagem: "",
+    idadePersonagem: "",
+    signo: "",
+    continente: "",
+    mestre: "",
+    reino: "",
+    Email: "",
+  });
+
+  const {
+    Nome,
+    telefone,
+    Idade,
+    conheceCDZ,
+    experiencia,
+    nomePersonagem,
+    idadePersonagem,
+    signo,
+    continente,
+    mestre,
+    reino,
+    Email,
+  } = data;
+
+  const handleCHange = (e) => {
+    setData({ ...data, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      
+      const response = await fetch(
+        "https://v1.nocodeapi.com/pedrohbs/google_sheets/nIFufKLpghzmUbjN?tabId=FichasNovas",
+        {
+          method: "POST",
+          
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify([
+            [
+              "",
+              new Date().toLocaleString(),
+              Nome,
+              telefone,
+              Idade,
+              conheceCDZ,
+              experiencia,
+              nomePersonagem,
+              "",
+              "",
+              "",
+              idadePersonagem,
+              signo,
+              continente,
+              "",
+              mestre,
+              "",
+              reino,
+              "",
+              "",
+              "",
+              "",
+              Email,
+            ],
+          ]),
+        }
+      );
+      await response.json();
+      setData({
+        ...data,
+              Nome,
+              telefone,
+              Idade,
+              conheceCDZ,
+              experiencia,
+              nomePersonagem,
+              idadePersonagem,
+              signo,
+              continente,
+              mestre,
+              reino,
+              Email,
+      });
+      document.location.href="https://chat.whatsapp.com/KiShRjXtQsZKBqs32zXQQ4";
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+
   return (
     <FormSection>
       <Container>
         <FormRow>
           <FormColumn small>
             <FormTitle>Registre-se</FormTitle>
-            <FormWrapper
-              action="https://formsubmit.co/818ad7196f53d6f5f5174da9228e0078"
-              method="POST"
-            >
-              <input type="hidden" name="_captcha" value="false" />
-              <input
-                type="hidden"
-                name="_next"
-                value="https://chat.whatsapp.com/KiShRjXtQsZKBqs32zXQQ4"
-              />
-              <input type="hidden" name="_template" value="table"></input>
+            <FormWrapper onSubmit={handleSubmit}>
               <FormRow>
                 <FormLabel>Possui experiência com RPG?</FormLabel>
-                <select id="experiencia" name="experiencia">
+                <select
+                  id="experiencia"
+                  name="experiencia"
+                  required
+                  value={experiencia}
+                  onChange={handleCHange}
+                >
+                  <option value="NãoEscolheu">------</option>
                   <option value="sim">Sim</option>
                   <option value="não">Não</option>
                 </select>
               </FormRow>
               <FormRow>
                 <FormLabel>Conehce cavaleiros do zodiaco?</FormLabel>
-                <select id="conheceCDZ" name="conheceCDZ">
+                <select
+                  id="conheceCDZ"
+                  name="conheceCDZ"
+                  required
+                  value={conheceCDZ}
+                  onChange={handleCHange}
+                >
+                  <option value="Não Escolheu">------</option>
                   <option value="sim">Sim</option>
                   <option value="não">Não</option>
                 </select>
@@ -46,22 +151,44 @@ const Form = () => {
                   type="text"
                   name="Nome"
                   placeholder="Seu Nome"
+                  value={Nome}
+                  required
+                  onChange={handleCHange}
+                ></FormInput>
+                <FormInput
+                  type="text"
+                  name="Idade"
+                  placeholder="Sua idade"
+                  value={Idade}
+                  required
+                  onChange={handleCHange}
                 ></FormInput>
                 <FormInput
                   type="email"
-                  name="E-mail"
+                  name="Email"
                   placeholder="Seu e-mail"
                   required
+                  value={Email}
+                  onChange={handleCHange}
                 ></FormInput>
                 <FormInput
                   type="tel"
                   name="telefone"
                   placeholder="Seu número de telefone "
                   required
+                  value={telefone}
+                  onChange={handleCHange}
                 ></FormInput>
 
                 <FormLabel>Escolha seu Reino</FormLabel>
-                <select id="reino" name="reino">
+                <select
+                  id="reino"
+                  name="reino"
+                  value={reino}
+                  required
+                  onChange={handleCHange}
+                >
+                  <option value="Não Escolheu">------</option>
                   <option value="Santuário">Santuário</option>
                   <option value="Submundo">Submundo</option>
                 </select>
@@ -71,6 +198,8 @@ const Form = () => {
                   name="nomePersonagem"
                   placeholder="Nome do seu Personagem"
                   required
+                  value={nomePersonagem}
+                  onChange={handleCHange}
                 ></FormInput>
 
                 <FormInput
@@ -78,10 +207,19 @@ const Form = () => {
                   name="idadePersonagem"
                   placeholder="Idade do seu Personagem"
                   required
+                  value={idadePersonagem}
+                  onChange={handleCHange}
                 ></FormInput>
 
                 <FormLabel>Escolha o continente do seu Personagem</FormLabel>
-                <select id="continente" name="continente">
+                <select
+                  id="continente"
+                  name="continente"
+                  value={continente}
+                  required
+                  onChange={handleCHange}
+                >
+                  <option value="Não Escolheu">------</option>
                   <option value="América">América</option>
                   <option value="Africa">Africa</option>
                   <option value="Europa">Europa</option>
@@ -94,10 +232,19 @@ const Form = () => {
                   type="text"
                   name="mestre"
                   placeholder="Nome do seu Mestre (Não obrigatorio)"
+                  value={mestre}
+                  onChange={handleCHange}
                 ></FormInput>
 
                 <FormLabel>Escolha o signo do seu Personagem</FormLabel>
-                <select id="signo" name="signo">
+                <select
+                  id="signo"
+                  name="signo"
+                  value={signo}
+                  required
+                  onChange={handleCHange}
+                >
+                  <option value="Não Escolheu">------</option>
                   <option value="Áries">Áries</option>
                   <option value="Touro">Touro</option>
                   <option value="Gêmeos">Gêmeos</option>
@@ -112,7 +259,9 @@ const Form = () => {
                   <option value="Peixes">Peixes</option>
                 </select>
               </FormRow>
-              <FormButton type="submit">Enviar</FormButton>
+              <FormButton type="submit" name="submit">
+                Enviar
+              </FormButton>
             </FormWrapper>
           </FormColumn>
         </FormRow>
