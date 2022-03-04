33789636a173854ebae70c31f32a50b9bef1be87
PedroHenrique431 <pedrohenriquebgs1@gmail.com>
2022-03-04 11:39:31 -0300

    .

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -192,8 +192,8 @@ const Form = () => {
                   <option value="Não Escolheu">------</option>
                   <option value="Santuário">Santuário</option>
                   <option value="Submundo">Submundo</option>
-                  <option value="Santuário">Asgard</option>
-                  <option value="Submundo">Atlântida</option>
+                  <option value="Asgard">Asgard</option>
+                  <option value="Atlântida">Atlântida</option>
                 </select>
 
                 <FormInput
